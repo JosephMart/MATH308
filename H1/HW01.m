39f2639b264c27a50657663c8be81cd8f7560a29
Joseph Martinsen <joseph@martinsen.com>
2017-02-04 14:03:35 -0600

    HW01 Ready for questions

--- H1/HW01.m
+++ H1/HW01.m
@@ -1,45 +1,42 @@
-% Problem Set A - 1, 2, 3, 9, 11
-clear;clc;
-% 1
-% a)
+%% Problem Set A - 1, 2, 3, 9, 11
+
+%% Problem 1
 A1 = 413/(768+295);
-fprintf('%.2f\n\n',A1)
+fprintf('a)\n\t %.2f\n\n',A1)
 
 % b)
-fprintf('%.2e\n',2^(123))
-fprintf('%.0f\n\n',2^(123))
+fprintf('b)\n\t%.2e\n',2^(123))
+fprintf('\t%.0f\n\n',2^(123))
 
 % c)
-fprintf('%1.34f\n',pi^2)
-fprintf('%1.34f\n\n',exp(1))
+fprintf('c)\n\t%1.34f\n',pi^2)
+fprintf('\t%1.34f\n\n',exp(1))
 
 % d)
 val = [61/88 13863/20000 253/365];
 ln2 = log(2);
 small = val(1);
 temp = ln2 + val(1);
-
+fprintf('d)\n')
 for i = 1:3
-    fprintf('%f\n',val(i))
+    fprintf('\t%f\n',val(i))
     if abs(val(i) - ln2) < temp
         small = val(i);
     end
 end
-fprintf('Best approx = %f\n\n',small)
+fprintf('\tBest approx = %f\n\n',small)
 
 
-
-% 2
+%% Problem 2
 a = 1;
 prob = ['a', 'b', 'c'];
-
 for i = 1:3
     val = sin(a/(10^i))/(a/10^i);
-    fprintf('%s) %.15f\n',prob(i),val)
+    fprintf('%s)\n\t %.15f\n',prob(i),val)
 end
 
 
-% 3
+%% Problem 3
 % a
 fplot(@(x) x.^3-x,[-1.5 1.5])
 title('3 A')
@@ -54,7 +51,7 @@ figure
 fplot(@(x) x.^3-x,[-2.5 2.5])
 title('3 C')
 
-% 9
+%% Problem 9
 figure
 fplot(@(x) x.^5 - 3*x.^2 + x + 1,[-2 2])
 syms x
@@ -64,7 +61,7 @@ ax = gca;
 ax.XAxisLocation = 'origin';
 ax.YAxisLocation = 'origin';
 
-% 11
+%% Problem 11
 syms x y
 e1 = x^2 - y^2 == 1;
 e2 = 2*x+y == 2;
