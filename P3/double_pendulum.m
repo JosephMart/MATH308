a4170ed0dd924aedf6132b0dbd1abe5d82bbf17e
Joseph Martinsen <joseph@martinsen.com>
2017-05-01 22:31:33 -0500

    Trying to plot stuff

--- P3/double_pendulum.m
+++ P3/double_pendulum.m
@@ -6,22 +6,23 @@ m2 = 1;
 L1 = 1;
 L2 = 2;
 
-u1=1.57;
-u1_prime=0;
-v1=3.14;
-v1_prime=0;
+u1       = 1.57;
+u1_prime = 0;
+v1       = 3.14;
+v1_prime = 0;
 
 [t,y]= ode45('pend_func' , [ 0 50], [ u1 ; u1_prime ; v1 ; v1_prime ]);
 
 % Potential Energy
 PE = -(m1+m2)*g*L1*cos(y(:,1))-m2*g*L2*cos(y(:,3));
 % Kinetic Energy
-KE = 0.5*m1*L1*L1*y(:,2).^2 + 0.5*m2*(L1*L1*y(:,2).^2+L2*L2*y(:,4).^2+2 * L1*L2*y(:,2).*y(:,4).*cos(y(:,1)-y(:,3)));
+KE = 1/2*m1*L1.^2*y(:,2).^2 + 0.5*m2*(L1.^2*y(:,2).^2+L2.^2*y(:,4).^2 + 2* L1*L2*y(:,2).*y(:,4).*cos(y(:,1)-y(:,3)));
 
 % Total Energy
 E=PE+KE;
 % Plot Total Energy
 plot(t ,E);
+figure
 
 % l o c a t i o n o f end p o i n t o f d o u bl e pendulum
 x = L1* sin( y(:,1)) + L2*sin(y(:,3));
@@ -38,6 +39,3 @@ figure;
 plot(t ,V);
 figure;
 plot(t, E);
-min(E)
-max(E)
-( max(E)-min(E))/max( E )
